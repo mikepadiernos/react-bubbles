156ba52e95fdc66bf485403101832189d212f9d4
Michael Padiernos <mike.padiernos@gmail.com>
2020-02-21 22:53:17 -0500

    FEATURE:(Utilities): Login form / page
    - created utilities axiosWithAuth.js & ProtectedRoute.js
    - coded login form that redirects to BubblePage.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,18 @@ import "./styles.scss";
 
 // IMPORT CONTEXTS
 import BubbleContext from "./contexts/BubbleContext";
+import LoggedContext from "./contexts/LoggedContext";
 
 // IMPORT UTILITIES
+import { axiosWithAuth } from "./utilities/axiosWithAuth";
+import ProtectedRoute from "./utilities/ProtectedRoute";
 
 // IMPORT APP COMPONENTS
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
 
 function App() {
+
   const colorItem               = {
     color:  "",
     code:   {
@@ -21,18 +26,29 @@ function App() {
   };
 
   const [bubbles, setBubbles]   = useState([]);
+  const [logged, setLogged] = useState( localStorage.getItem("token") );
 
   return (
     <Router>
-      <BubbleContext.Provieder value={{ bubbles, setBubbles, colorItem }}>
+      <BubbleContext.Provider value={{ bubbles, setBubbles, colorItem }}>
+        <LoggedContext.Provider value={{ logged, setLogged }}>
         <div className="App">
-          <Route exact path="/" component={Login} />
-          {/*
-            Build a PrivateRoute component that will
-            display BubblePage when you're authenticated
-          */}
+          <div className="container">
+            <header role="header" id="header" className="header">
+              <h1>Welcome to the Bubble App!</h1>
+            </header>
+            <main role="content" id="main-content" className="main-content">
+              <Route exact path="/" component={Login} />
+              <ProtectedRoute path="/bubbles" component={BubblePage}/>
+            </main>
+            {/*
+              Build a PrivateRoute component that will
+              display BubblePage when you're authenticated
+            */}
+          </div>
         </div>
-      </BubbleContext.Provieder>
+        </LoggedContext.Provider>
+      </BubbleContext.Provider>
     </Router>
   );
 }
